fd16d4559ed5b95676af959e293caad527d023fb
vokur13 <vokur13@gmail.com>
2022-09-26 10:14:24 +0300

    fixed

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
-// import { SectionTitle } from './SectionTitle.styled';
 
 export const Section = ({ title, children }) => {
   return (
